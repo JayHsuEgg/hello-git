80464d8cd92237e09cb685850580e70db2382064
JayHsuEgg <jay.hsu.egg@gmail.com>
2020-04-11 21:15:36 +0800

    1st title
    4th Commit, 3rd Change

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-print("Hello, This is the origin version!")
+print("Hello, This is the origin version! Here, Comes Third Change!")
 
-print("Here, Comes First Change! Here, Comes Second Change! ")
+print("Here, Comes First Change! Here, Comes Second Change!")
