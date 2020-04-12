44f8a86c3cf13cc689913ab32f08ff3d79f282d7
JayHsuEgg <jay.hsu.egg@gmail.com>
2020-04-12 15:01:37 +0800

    8th Commit, 1st Upload Change

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ print("Hello, This is the origin version! Here, Comes Third Change!")
 print("Here, Comes First Change! Here, Comes Second Change!")
 
 print("Vim try")
+
+print("After upload to github change")
